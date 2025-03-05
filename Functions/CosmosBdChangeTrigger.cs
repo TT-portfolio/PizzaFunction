1297d8090334807c9e88c08f009fcc9ceadb18af
Thomas Hallström <minimoto@hotmail.com>
2025-03-05 15:21:59 +0100

    Update CosmosDbChangeTriggers

---
+++ Functions/CosmosBdChangeTrigger.cs
@@ -0,0 +1,58 @@
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using System.Text.Json;
+
+namespace PizzaFunction.Functions
+{
+    public class CosmosBdChangeTrigger
+    {
+        private readonly ILogger _logger;
+
+        public CosmosBdChangeTrigger(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<CosmosBdChangeTrigger>();
+        }
+
+        [Function("CosmosBdChangeTrigger")]
+        [SignalROutput(HubName = "orders", ConnectionStringSetting = "AzureSignalRConnectionString")]
+        public SignalRMessageAction Run(
+            [CosmosDBTrigger(
+            databaseName: "Resturant",
+            containerName: "Orders",
+            Connection = "CosmosDBConnection",
+            LeaseContainerName = "leases",
+            CreateLeaseContainerIfNotExists = true)] IReadOnlyList<MyDocument> input)
+        {
+
+            if (input != null && input.Count > 0)
+            {
+
+                _logger.LogInformation($"Documents modified: {input.Count}");
+                _logger.LogInformation($"First document Id: {input[0].id}");
+                _logger.LogInformation($"Skickar uppdatering till SignalR: {JsonSerializer.Serialize(input[0])}");
+
+                _logger.LogInformation($"Försöker skicka SingalRMess");
+
+
+                return new SignalRMessageAction("orderUpdated")
+                {
+
+                    Arguments = new[] { input[0] }
+                };
+
+            }
+            return null;
+        }
+    }
+
+    public class MyDocument
+    {
+        public string id { get; set; }
+
+        public string Text { get; set; }
+
+        public int Number { get; set; }
+
+        public bool Boolean { get; set; }
+    }
+}
